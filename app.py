62ade95785bf7dc46b1b8e61014df47bce45ff32
55RoronoaZoro55 <windking2013@gmail.com>
2026-05-30 13:16:51 +0300

    app.py rotalari duzenlendi

--- app.py
+++ app.py
@@ -1,4 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# Ana sayfa (index.html)
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# Giriş sayfası (login.html)
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+# Kayıt sayfası (signup.html)
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+# Gizlilik sayfası (privacy.html)
+@app.route('/privacy')
+def privacy():
+    return render_template('privacy.html')
+
+# Kullanım koşulları sayfası (terms.html)
+@app.route('/terms')
+def terms():
+    return render_template('terms.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
